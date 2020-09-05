c7b907753afc66bdeb7bdbacb5b3ca593dbbcc93
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-05 08:05:32 +0700

    client only

--- pages/home.vue
+++ pages/home.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
     <div class="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="flex flex-col space-y-4">
-        <div
-          class="p-2 border border-red-200 group hover:border-red-400 duration-500 rounded-md"
-          v-for="ad in allAds"
-          :key="ad.id"
-        >
-          <nuxt-link
-            :to="{ name: 'iklan-slug', params: { slug: ad.slug } }"
-            class="text-xl font-medium leading-snug text-red-500 group-hover:text-red-600"
+      <client-only>
+        <div class="flex flex-col space-y-4">
+          <div
+            class="p-2 duration-500 border border-red-200 rounded-md group hover:border-red-400"
+            v-for="ad in allAds"
+            :key="ad.id"
           >
-            {{ ad.title }}
-          </nuxt-link>
-          <div class="flex mt-2 justify-start">
-            <p class="text-xs">
-              {{ ad.user.name }}
-            </p>
+            <nuxt-link
+              :to="{ name: 'iklan-slug', params: { slug: ad.slug } }"
+              class="text-xl font-medium leading-snug text-red-500 group-hover:text-red-600"
+            >
+              {{ ad.title }}
+            </nuxt-link>
+            <div class="flex justify-start mt-2">
+              <p class="text-xs">
+                {{ ad.user.name }}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </client-only>
     </div>
   </div>
 </template>
