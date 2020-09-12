9bb8d76c1c0148e38d4e1120d129537002161e81
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-12 07:30:02 +0700

    large

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header />
-    <div class="max-w-xl p-4 mx-auto text-gray-800">
+    <div class="max-w-5xl p-4 mx-auto text-gray-800">
       <nuxt />
     </div>
   </div>
--- pages/home.vue
+++ pages/home.vue
@@ -1,61 +1,38 @@
 <template>
   <div>
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div>
-        <h1 class="text-3xl font-bold text-red-500">Dipromosikan</h1>
-        <div class="flex flex-wrap mt-4">
-          <div
-            class="flex flex-col justify-start w-1/2 p-1 duration-500 rounded-md sm:w-1/3 hover:shadow-md hover:border group hover:border-red-400"
-            v-for="ad in ads.data"
-            :key="ad.id"
-          >
-            <nuxt-link :to="{ name: 'iklan-slug', params: { slug: ad.slug } }">
-              <div class="w-full h-32 bg-red-400 rounded-md"></div>
-              <div class="p-2">
-                <div
-                  class="text-xl font-medium leading-snug text-red-500 group-hover:text-red-600"
-                >
-                  Rp. {{ ad.price }}
-                </div>
-                <div class="mt-1">
-                  <p class="font-medium">
-                    {{ ad.title }}
-                  </p>
-                  <p class="mt-1 text-xs">
-                    {{ ad.user.name }}
-                  </p>
-                </div>
-              </div>
-            </nuxt-link>
-          </div>
-        </div>
-      </div>
+    <div class="sm:px-6 lg:px-8">
       <div class="mt-4">
         <h1 class="text-3xl font-bold text-red-500">Terbaru</h1>
         <div class="flex flex-wrap mt-4">
           <div
-            class="flex flex-col justify-start w-1/2 p-1 duration-500 rounded-md sm:w-1/3 hover:shadow-md hover:border group hover:border-red-400"
+            class="flex flex-col justify-start w-1/2 my-2 duration-500 sm:w-1/3 md:w-1/4"
             v-for="ad in ads.data"
             :key="ad.id"
           >
-            <nuxt-link :to="{ name: 'iklan-slug', params: { slug: ad.slug } }">
-              <div class="w-full h-32 bg-red-400 rounded-md"></div>
-              <div class="p-2">
-                <div
-                  class="text-xl font-medium leading-snug text-red-500 group-hover:text-red-600"
-                >
-                  Rp. {{ ad.price }}
+            <div
+              class="p-2 rounded-md hover:shadow-md hover:border group hover:border-red-400"
+            >
+              <nuxt-link
+                :to="{ name: 'iklan-slug', params: { slug: ad.slug } }"
+              >
+                <div class="w-full h-32 bg-red-400 rounded-md"></div>
+                <div class="p-2">
+                  <div
+                    class="text-xl font-medium leading-snug text-red-500 group-hover:text-red-600"
+                  >
+                    Rp. {{ ad.price }}
+                  </div>
+                  <div class="mt-1">
+                    <p class="font-medium">
+                      {{ ad.title }}
+                    </p>
+                    <p class="mt-1 text-xs">
+                      {{ ad.user.name }}
+                    </p>
+                  </div>
                 </div>
-                <div class="mt-1">
-                  <p class="font-medium">
-                    {{ ad.title }}
-                  </p>
-                  <p class="mt-1 text-xs">
-                    {{ ad.user.name }}
-                  </p>
-                </div>
-              </div>
-            </nuxt-link>
+              </nuxt-link>
+            </div>
           </div>
         </div>
       </div>
