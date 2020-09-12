cb99ec16a2ff194ece47180443518c2f50662591
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-12 10:12:53 +0700

    responsive

--- pages/iklan/_slug.vue
+++ pages/iklan/_slug.vue
@@ -1,45 +1,58 @@
 <template>
   <div>
-    <div>
-      <div>
-        <img class="object-cover w-full h-64 rounded-md" :src="ad.cover_image.url.medium" />
+    <div class="md:flex md:space-x-4">
+      <div class="md:w-1/2">
+        <img
+          class="object-cover w-full h-64 rounded-md"
+          :src="ad.cover_image.url.medium"
+        />
+        <div class="flex py-2 space-x-2">
+          <img
+            v-for="image in ad.images"
+            :key="image.id"
+            class="object-cover w-16 h-16 rounded-md"
+            :src="image.url.small"
+          />
+        </div>
       </div>
-      <h1 class="py-2 text-3xl font-bold text-grey-800">
-        {{ ad.title }}
-      </h1>
-      <div class="py-2">
-        <p class="font-medium text-red-500">Harga</p>
-        <p class="mt-2">
-          {{
-            ad.price.toLocaleString('id', {
-              style: 'currency',
-              currency: 'IDR',
-            })
-          }}
-        </p>
-      </div>
-      <div class="py-2">
-        <p class="font-medium text-red-500">Deksripsi</p>
-        <p class="mt-2">{{ ad.detail }}</p>
-      </div>
-      <div class="py-2">
-        <p class="font-medium text-red-500">Kategori</p>
-        <p class="mt-2">{{ ad.category.name }}</p>
-      </div>
-    </div>
-    <div class="py-2">
-      <div>
-        <p class="font-medium text-red-500">Hubungi kontak berikut</p>
-        <p>Nama: {{ ad.user.name }}</p>
-        <p>Email: {{ ad.user.email }}</p>
-      </div>
-      <div v-if="!$auth.loggedIn" class="mt-8">
-        <nuxt-link
-          to="/login"
-          class="block px-4 py-2 text-center text-white duration-500 bg-red-500 rounded-md hover:bg-red-600"
-        >
-          Mau buat iklan seperti ini?
-        </nuxt-link>
+      <div class="md:w-1/2">
+        <h1 class="py-2 text-3xl font-bold text-grey-800">
+          {{ ad.title }}
+        </h1>
+        <div class="py-2">
+          <p class="font-medium text-red-500">Harga</p>
+          <p class="mt-2">
+            {{
+              ad.price.toLocaleString('id', {
+                style: 'currency',
+                currency: 'IDR',
+              })
+            }}
+          </p>
+        </div>
+        <div class="py-2">
+          <p class="font-medium text-red-500">Deksripsi</p>
+          <p class="mt-2">{{ ad.detail }}</p>
+        </div>
+        <div class="py-2">
+          <p class="font-medium text-red-500">Kategori</p>
+          <p class="mt-2">{{ ad.category.name }}</p>
+        </div>
+        <div class="py-2">
+          <div>
+            <p class="font-medium text-red-500">Hubungi kontak berikut</p>
+            <p>Nama: {{ ad.user.name }}</p>
+            <p>Email: {{ ad.user.email }}</p>
+          </div>
+          <div v-if="!$auth.loggedIn" class="mt-8">
+            <nuxt-link
+              to="/login"
+              class="block px-4 py-2 text-center text-white duration-500 bg-red-500 rounded-md hover:bg-red-600"
+            >
+              Mau buat iklan seperti ini?
+            </nuxt-link>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -52,9 +65,14 @@ export default {
       title: this.ad.title || 'Detail iklan',
       meta: [
         {
+          hid: 'title',
+          name: 'title',
+          content: this.ad.title,
+        },
+        {
           hid: 'description',
           name: 'description',
-          content: 'Detail Nuxt.js ',
+          content: this.ad.detail,
         },
       ],
     }
