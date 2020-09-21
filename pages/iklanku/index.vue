18d4d8a73b12402cc707c927af5883621baa239f
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-21 08:51:14 +0700

    dropdown

--- pages/iklanku/index.vue
+++ pages/iklanku/index.vue
@@ -12,31 +12,139 @@
             <p>Tanggal terbit: {{ $moment(item.time_start).format('LLLL') }}</p>
             <p>Tanggal selesai: {{ $moment(item.time_end).format('LLLL') }}</p>
           </div>
-          <div class="flex justify-between mt-2 text-sm">
-            <div>
+          <div class="flex justify-between mt-4 text-sm">
+            <div class="flex space-x-2">
               <nuxt-link
                 :to="{ name: 'item-slug', params: { slug: item.slug } }"
-                class="inline-block px-4 py-2 text-gray-700 transition duration-500 border rounded hover:border-transparent hover:bg-gray-200"
+                class="px-4 py-2 text-gray-700 transition duration-500 border rounded hover:border-transparent hover:bg-gray-200"
               >
-                Lihat
-              </nuxt-link>
-              <nuxt-link
-                :to="{
-                  name: 'iklanku-slug-edit',
-                  params: { slug: item.slug },
-                }"
-                class="inline-block px-4 py-2 text-gray-700 transition duration-500 border rounded hover:border-transparent hover:bg-gray-200"
-              >
-                Edit
+                Lihat iklan ini
               </nuxt-link>
             </div>
             <div>
-              <button
-                @click="deleteAd(item)"
-                class="inline-block px-4 py-2 text-red-500 transition duration-500 border rounded hover:border-transparent hover:bg-red-200"
-              >
-                Hapus
-              </button>
+              <div class="relative inline-block text-left">
+                <client-only>
+                  <div>
+                    <span class="rounded-md shadow-sm">
+                      <button
+                        type="button"
+                        class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                        id="options-menu"
+                        aria-haspopup="true"
+                        aria-expanded="true"
+                        @click="
+                          dropdownItemIdOpen === item.id
+                            ? (dropdownItemIdOpen = 0)
+                            : (dropdownItemIdOpen = item.id)
+                        "
+                      >
+                        Options
+                        <!-- Heroicon name: chevron-down -->
+                        <svg
+                          class="w-5 h-5 ml-2 -mr-1"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </span>
+                  </div>
+
+                  <!--
+    Dropdown panel, show/hide based on dropdown state.
+
+    Entering: "transition ease-out duration-100"
+      From: "transform opacity-0 scale-95"
+      To: "transform opacity-100 scale-100"
+    Leaving: "transition ease-in duration-75"
+      From: "transform opacity-100 scale-100"
+      To: "transform opacity-0 scale-95"
+  -->
+                  <transition
+                    enter-active-class="transition duration-100 ease-out"
+                    enter-class="transform scale-95 opacity-0"
+                    enter-to-class="transform scale-100 opacity-100"
+                    leave-active-class="transition duration-75 ease-in"
+                    leave-class="transform scale-95 opacity-0"
+                    leave-to-class="transform scale-95 opacity-0"
+                  >
+                    <div
+                      v-show="dropdownItemIdOpen === item.id"
+                      class="absolute right-0 z-10 w-56 mt-2 origin-top-right rounded-md shadow-lg"
+                    >
+                      <div
+                        class="bg-white rounded-md shadow-xs"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        <div class="py-1">
+                          <nuxt-link
+                            :to="{
+                              name: 'iklanku-slug-edit',
+                              params: { slug: item.slug },
+                            }"
+                            class="block w-full px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Edit</nuxt-link
+                          >
+                          <a
+                            href="#"
+                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Buat lagi seperti ini</a
+                          >
+                        </div>
+                        <div class="border-t border-gray-100"></div>
+                        <div class="py-1">
+                          <a
+                            href="#"
+                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Batal terbit</a
+                          >
+                          <a
+                            href="#"
+                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Terbit ulang (sundul)</a
+                          >
+                        </div>
+                        <div class="border-t border-gray-100"></div>
+                        <div class="py-1">
+                          <a
+                            href="#"
+                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Bagikan</a
+                          >
+                          <a
+                            href="#"
+                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                            >Add to favorites</a
+                          >
+                        </div>
+                        <div class="border-t border-gray-100"></div>
+                        <div class="py-1">
+                          <button
+                            @click="deleteAd(item)"
+                            class="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                          >
+                            Hapus
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </transition>
+                </client-only>
+              </div>
             </div>
           </div>
         </div>
@@ -84,6 +192,7 @@ export default {
   middleware: 'auth',
   data() {
     return {
+      dropdownItemIdOpen: 0,
       items: {
         data: [],
         pagination: '',
